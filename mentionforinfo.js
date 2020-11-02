1617160710c215cf0a390fee5e5436d3712e06d2
francyespo16 <38110096+francyespo16@users.noreply.github.com>
2020-11-02 17:08:31 +0100

    Create mentionforinfo.js

---
+++ mentionforinfo.js
@@ -0,0 +1,9 @@
+if(msg.content=='<@!USERID>'){
+    
+    const mentioninfo = new Discord.MessageEmbed()
+    .setColor('#34eb46')
+    .setTitle("TESTO")
+    msg.channel.send(mentioninfo );
+
+
+  } 
